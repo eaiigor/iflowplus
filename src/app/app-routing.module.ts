3a083144cc93a9cd59491080bcc7be35f4fd79c1
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2023-12-02 11:11:52 -0300

    New components / navigate function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from './components/start/start.component';
+import { SearchComponent } from './components/search/search.component';
+import { MyListComponent } from './components/my-list/my-list.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StartComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'my-list', component: MyListComponent },
+  { path: 'movie/:id', component: MovieDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { StartComponent } from './components/start/start.component';
+import { SearchComponent } from './components/search/search.component';
+import { MyListComponent } from './components/my-list/my-list.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StartComponent,
+    SearchComponent,
+    MyListComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
